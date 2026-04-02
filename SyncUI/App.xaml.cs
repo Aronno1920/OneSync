f65d17cb3ad5d79405192dfcdbfc71fb5de2eb3f
19littleghost <aronno1920@hotmail.com>
2026-04-03 01:52:35 +0600

    Update project only for Windows PC

---
+++ SyncUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SyncUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SyncUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SyncUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
